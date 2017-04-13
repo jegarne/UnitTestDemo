d03d8431ac5cb6124e073f897ee64c6d93b995df
Jeremy Garner <jegarne@gmail.com>
2017-04-13 18:37:51 -0500

    Add unit test project

---
+++ UnitTestDemoTests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobs.Data;
+
+namespace UnitTestDemoTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void JobData_JobAreNotNull()
+        {
+            JobData jobData = new JobData();
+            var jobs = jobData.Jobs;
+
+            Assert.IsNotNull(jobs);
+        }
+
+        [TestMethod]
+        public void JobData_FindByValue_ContainsNewYork()
+        {
+            JobData jobData = new JobData();
+            var jobs = jobData.FindByValue("New York");
+
+            Assert.AreEqual(1, jobs.Count);            
+        }
+    }
+}
